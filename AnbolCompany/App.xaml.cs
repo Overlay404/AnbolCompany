547cbacccdc50e2c5d781d219cbad8db0c93d4fb
Overlay404 <denis.alekseev.200411@gmail.com>
2022-12-01 15:05:47 +0300

    Добавьте файлы проекта.

---
+++ AnbolCompany/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AnbolCompany.Resourses;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AnbolCompany
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static AnbolEntities db = new AnbolEntities();
+        public App()
+        {
+
+        }
+    }
+}
---
+++ AnbolCompany/ThemeChange.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AnbolCompany
+{
+    internal class ThemeChange
+    {
+        public static bool position { get; set; }
+        public static void Change()
+        {
+            if (ThemeChange.position)
+            {
+                var uri = new Uri(@"Style/WhiteTheme.xaml", UriKind.Relative);
+                ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+                Application.Current.Resources.Clear();
+                Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+                ThemeChange.position = false;
+            }
+            else
+            {
+                var uri = new Uri(@"Style/DarkTheme.xaml", UriKind.Relative);
+                ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+                Application.Current.Resources.Clear();
+                Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+                ThemeChange.position = true;
+            }
+        }
+        public static void ShowTheme()
+        {
+            if (!ThemeChange.position)
+            {
+                var uri = new Uri(@"Style/WhiteTheme.xaml", UriKind.Relative);
+                ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+                Application.Current.Resources.Clear();
+                Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+                ThemeChange.position = false;
+            }
+            if (ThemeChange.position)
+            {
+                var uri = new Uri(@"Style/DarkTheme.xaml", UriKind.Relative);
+                ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+                Application.Current.Resources.Clear();
+                Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+                ThemeChange.position = true;
+            }
+        }
+    }
+}
