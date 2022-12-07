2d09062beca6a6b0c23f8811855e06ddb00376bd
Overlay404 <denis.alekseev.200411@gmail.com>
2022-12-07 13:09:34 +0300

    CollectionView 1.1

--- AnbolCompany/Products.xaml.cs
+++ AnbolCompany/Products.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using AnbolCompany.Resourses;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AnbolCompany
 {
@@ -25,7 +15,7 @@ namespace AnbolCompany
     {
         static int thisPage1 = 1;
         CollectionView products { get; set; }
-        
+
         public static Products Instance { get; set; }
         public Products()
         {
@@ -33,13 +23,13 @@ namespace AnbolCompany
 
             Instance = this;
 
-            products = (CollectionView)CollectionViewSource.GetDefaultView(listProducts.ItemsSource);
+            listProducts.ItemsSource = App.db.Products.ToList();
+
             quantity.SelectedIndex = 0;
             filtration.SelectedIndex = 0;
             sorting.SelectedIndex = 0;
 
-            listProducts.ItemsSource = App.db.Products.ToList();
-
+            Update();
             MainWindow.Instance.plusImage.MouseUp += PlusImage_MouseUp;
             MainWindow.Instance.editImage.MouseUp += EditImage_MouseUp;
         }
@@ -78,12 +68,13 @@ namespace AnbolCompany
 
         private void quantity_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+
         }
 
         void Update()
         {
-            
+            products = (CollectionView)CollectionViewSource.GetDefaultView(listProducts.ItemsSource);
+            products.SortDescriptions.Add(new SortDescription("nameProduct", ListSortDirection.Ascending));
         }
     }
 }
